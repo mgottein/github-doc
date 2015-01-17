1da3ef3cb45fcb348c01e144dba77242bd696340
Dan Green <dan.singkiwi@gmail.com>
2015-01-16 21:44:13 -0500

    Test folder
    added testapp folder and runnable

---
+++ runnable.py
@@ -0,0 +1,14 @@
+'''
+Created on Jan 16, 2015
+
+@author: Dan Green
+'''
+
+import os
+
+def collateData(repodir):
+    print repodir
+
+if __name__ == '__main__':
+    repodir = 'C:\Users\Dan\Documents\GitHub\github-doc\testapp'
+    collateData(repodir)
