5a061faaec649c37c57ae5c57e7c5f04be33ba90
Matthew Gotteiner <matthew.gotteiner@gmail.com>
2015-01-16 21:57:07 -0500

    rename

---
+++ javadoc_parser.py
@@ -0,0 +1,47 @@
+import re
+
+tagRegexp = re.compile(r'\s*\**\s*@.*')
+
+def isLineTag(line):
+    return tagRegexp.match(line)
+
+class Tag:
+    def __init__(self, text):
+        atIndex = text.index('@')
+
+class Comment:
+
+javadocRegexp = re.compile(r'/\*\*.*?\*/', re.DOTALL)
+
+def getJavadocText(f):
+    java = f.read();
+    javadocs = javadocRegexp.findall(java)
+    return javadocs
+
+
+def extractTags(javadocs):
+    extractedTags = []
+    extractedText = []
+    for javadoc in javadocs:
+        tags = []
+        text = []
+        lines = javadoc.split('\n')
+        i = 1
+        while i < len(lines):
+            line = lines[i]
+            if tagRegexp.match(line):
+                tags.append((i, line))
+            else:
+                text.append((i, line))
+            i = i + 1
+        extractedTags.append(tags)
+        extractedText.append(text)
+    return (extractedTags, extractedText)
+
+javadocs = getJavadocs(open('testapp/Test.java', 'r'))
+
+extracted = extractTags(javadocs)
+print "Tags"
+print extracted[0]
+print "Text"
+print extracted[1]
